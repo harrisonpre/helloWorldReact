4e36f11f1d7dc8baf25f436108aae0484800a0a0
Harrison Preusse <hpreusse@gmail.com>
2019-05-23 13:09:28 -0700

    hello world! 5/23

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      appTitle: "Hello World",
+      appMessage: "My very first app in React"
+    };  
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">{this.state.appTitle}</h1>
+          <p>
+            {this.state.appMessage}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
